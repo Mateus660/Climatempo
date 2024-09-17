81f427c61ff49216a1c670001fe7434c326c8ffa
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2024-09-17 04:07:22 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const key = " ad6dceb519ab98d72783812f6818e44b"
+const key = "ad6dceb519ab98d72783812f6818e44b"
+
+function colocarDadosNaTela(dados) {
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°C";
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "% de Umidade"
+   
+}
 
 async function buscarCidade(cidade) {
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
 
-    console.log(dados)
+    colocarDadosNaTela(dados)
      
 }
 
